e66f16157e7caf9396f2ca52aff45def91c83c61
Kayle <kayle.barrt@gmail.com>
2023-09-23 00:26:44 -0300

    feat(add): install uuid

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,7 +3,7 @@ import HomeView from '../views/HomeView.vue'
 import CreateTodoView from '../views/CreateTodoView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     {
       path: '/',
